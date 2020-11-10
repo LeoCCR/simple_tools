c1efe3ddf31ef21acf822978024d6c8eff34965d
LeoCCR <private@private.com>
2020-11-10 17:42:56 +0800

    Create list_csv function.

---
+++ list_csv.py
@@ -0,0 +1,18 @@
+def list_csv(path):
+    """Return a list of .csv file path from .csv file path or folder that contain many .csv file.
+
+    Args:
+        path ([string]): .csv file path or folder path that contain many .csv file.
+
+    Returns:
+        [list]: A python list that contain .csv file.
+    """
+
+    from os import listdir
+    from os.path import isdir, isfile, join
+
+    if ".csv" in path and isfile(path):
+        return [path]
+
+    elif isdir(path):
+        return [join(path, file) for file in listdir(path) if ".csv" in file]
